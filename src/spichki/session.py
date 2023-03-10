cd17da590c7909ac34537928305132d34154951b
SciBourne <bourne-sci-hack@yandex.ru>
2023-03-10 17:52:53 +0500

    REFACTOR: Replace attrs to read-only propetries
    [ Public ]:
    
     - GameSession().game: Game()
     - GameSession().user_name: str
     - GameSession().bot_name: str

--- src/spichki/session.py
+++ src/spichki/session.py
@@ -3,13 +3,25 @@ from spichki.models import Game, Player
 
 class GameSession:
     def __init__(self, user_name: str, bot_name: str):
-        self.game: 'Game' = Game()
+        self.__game: 'Game' = Game()
 
-        self.user: 'Player' = Player(
-            game=self.game,
+        self.__user: 'Player' = Player(
+            game=self.__game,
             name=user_name
         )
-        self.bot: 'Player' = Player(
-            game=self.game,
+        self.__bot: 'Player' = Player(
+            game=self.__game,
             name=bot_name
         )
+
+        @property
+        def game(self):
+            return self.__game
+
+        @property
+        def user_name(self):
+            return self.__user.name
+
+        @property
+        def bot_name(self):
+            return self.__bot.name
